3da4a4d4fad85374a02264eab8078db768fd1b87
SANGHOON LIM <shlim@kiaf-ui.sdfarm.kr>
2020-02-21 07:43:57 +0900

    Initial commit

---
+++ Software/IPGlasma-KIAF/ipglasma/src/Util.h
@@ -0,0 +1,17 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+
+namespace Util{
+    double *vector_malloc(int);
+    char *char_malloc(int);
+    void char_free(char *);
+    void vector_free(double *);
+    int IsFile(std::string);
+    std::string stringFind(std::string file_name, std::string st);
+    double DFind(std::string file_name, std::string st);
+    int IFind(std::string file_name, std::string st);
+};
+
+#endif
