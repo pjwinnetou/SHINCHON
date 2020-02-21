24279ab9fde4d595e8ebc2b65f74a252138fd136
Sanghoon Lim <shlim@rcf.rhic.bnl.gov>
2020-02-21 10:17:40 +0900

    Initial commit

--- Software/SONIC-KIAF/macro/Draw_time.C
+++ Software/SONIC-KIAF/macro/Draw_time.C
@@ -1,30 +1,24 @@
-#include "/phenix/u/shlim/Style.h"
-
-void Draw_time(const int grp=49, const int run=0){
+void Draw_time(){
 
 	gStyle->SetPalette(55);
 	gStyle->SetOptStat(0);
 
-	const bool bLOG = true;
+	const bool bLOG = false;
 
 	//const int ntime = 87;
-	const int ntime = 172;
+	//const int ntime = 172;
 
-	const float Qs = 1.0;
-	const float m = 0.0;
+	TFile *infile = new TFile("outfile.root","read");
 
-	//TFile *infile = new TFile(Form("running_grp%d/IPGlasma_flat_useNucleus0_grid1024_g2mu0.025_m0.15_run%05d.root",grp,run),"read");
-	//TFile *infile = new TFile(Form("running_grp%d/IPGlasma_flat_useNucleus0_grid1024_g2mu0.10_m0.15_run%05d.root",grp,run),"read");
-	//TFile *infile = new TFile(Form("running_grp%d/IPGlasma_dAu_useNucleus1_grid256_g2mu1.60_m0.0_run%05d.root",grp,run),"read");
-	TFile *infile = new TFile(Form("running_grp%d/IPGlasma_dAu_useNucleus1_grid512_g2mu0.40_m0.0_run%05d.root",grp,run),"read");
+	TH1D *h1_time = (TH1D*)infile->Get("h1_time");
+
+	const int ntime = int(h1_time->GetEntries());
 
 	TH2D *h2d[ntime];
 
 	for (int it=0; it<ntime; it++){
-		h2d[it] = (TH2D*)infile->Get(Form("h2_evt%05d_t%05d",run,it));
+		h2d[it] = (TH2D*)infile->Get(Form("h2_evt%05d_t%05d",0,it));
 	}
-
-	TH1D *h1_time = (TH1D*)infile->Get("h1_time");
 
 	float max = h2d[0]->GetMaximum();
 	float min = h2d[ntime-1]->GetMinimum();
@@ -33,10 +27,11 @@ void Draw_time(const int grp=49, const int run=0){
 	TCanvas *c1 = new TCanvas("c1","c1",1.2*1*500,500);
 
 	//c1->cd(1);
-	SetPadStyle();
-	gPad->SetRightMargin(0.135);
-	gPad->SetLeftMargin(0.13);
+	//SetPadStyle();
+	gPad->SetRightMargin(0.13);
+	gPad->SetLeftMargin(0.09);
 	gPad->SetTopMargin(0.08);
+	gPad->SetBottomMargin(0.1);
 	if ( bLOG ) gPad->SetLogz();
 
 	/*
@@ -49,7 +44,7 @@ void Draw_time(const int grp=49, const int run=0){
 	*/
 
 
-	gSystem->Exec(Form("mkdir -p running_grp%d/gif_run%05d",grp,run));
+	gSystem->Exec("mkdir -p gif/gif_run00");
 
 	for (int it=0; it<ntime; it++){
 	//for (int it=0; it<1; it++){
@@ -59,37 +54,61 @@ void Draw_time(const int grp=49, const int run=0){
 		//c1->cd(2);
 		//c1->Clear();
 
-		if ( h2d[it]->Integral()<0.01*integral ) continue;
+		//if ( h2d[it]->Integral()<0.01*integral ) continue;
 
 		//htmp = (TH1D*)gPad->DrawFrame(-15,-15,15,15);
-		htmp = (TH1D*)gPad->DrawFrame(-7.5,-7.5,7.5,7.5);
-		SetHistoStyle("x [fm]", "y [fm]");
+		TH1D *htmp = (TH1D*)gPad->DrawFrame(-10.,-10.,10.,10.);
+		htmp->GetXaxis()->SetTitle("x [fm]");
+		htmp->GetXaxis()->SetLabelFont(43);
+		htmp->GetXaxis()->SetLabelSize(20);
+		htmp->GetXaxis()->SetTitleFont(43);
+		htmp->GetXaxis()->SetTitleSize(20);
+
+		htmp->GetYaxis()->SetTitle("y [fm]");
+		htmp->GetYaxis()->SetLabelFont(43);
+		htmp->GetYaxis()->SetLabelSize(20);
+		htmp->GetYaxis()->SetTitleFont(43);
+		htmp->GetYaxis()->SetTitleSize(20);
+
 		htmp->GetXaxis()->CenterTitle();
 		htmp->GetYaxis()->CenterTitle();
 		htmp->GetYaxis()->SetTitleOffset(1.0);
+
+		htmp->GetZaxis()->SetLabelFont(43);
+		htmp->GetZaxis()->SetLabelSize(20);
+		htmp->GetZaxis()->SetTitleFont(43);
+		htmp->GetZaxis()->SetTitleSize(20);
 
 		if ( bLOG ){
 			h2d[it]->SetMinimum(min);
 			h2d[it]->SetMaximum(max);
 		}else{
-			h2d[it]->SetMaximum(1.02*max);
+			h2d[it]->SetMaximum(1.05*max);
 			h2d[it]->SetMinimum(0);
 		}
 		htmp = (TH1D*)h2d[it];
-		SetHistoStyle("","","",0.04,0.04);
+		//SetHistoStyle("","","",0.04,0.04);
 
+		TLatex *tex = new TLatex(10.,10.5,"T [GeV]");
+		tex->SetTextFont(43);
+		tex->SetTextSize(20);
+		//tex->SetTextAlign(21);
+		tex->Draw();
+
+		/*
 		float time = h1_time->GetBinContent(it);
 		TLatex *tex = new TLatex(0,6.5,Form("Q_{s}=%g GeV, m=%g GeV, t=%7.5f fm/c",Qs,m,time));
 		tex->SetTextFont(63);
 		tex->SetTextSize(20);
 		tex->SetTextAlign(21);
 		tex->Draw();
+		*/
 
 		h2d[it]->Draw("colz same");
 		if ( bLOG ){
-			c1->SaveAs(Form("running_grp%d/gif_run%05d/time_log_%05d.gif",grp,run,it));
+			c1->SaveAs(Form("gif/gif_run00/time_log_%05d.gif",it));
 		}else{
-			c1->SaveAs(Form("running_grp%d/gif_run%05d/time_linear_%05d.gif",grp,run,it));
+			c1->SaveAs(Form("gif/gif_run00/time_linear_%05d.gif",it));
 		}
 	}
 
