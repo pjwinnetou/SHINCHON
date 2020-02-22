0cc2ba8eed967f368a4a54f62a4c2dc8fa4553db
Sanghoon Lim <shlim1986@gmail.com>
2020-02-22 14:05:03 +0900

    Update for ROOT5

--- Software/MCGlauber/Run_glauber.C
+++ Software/MCGlauber/Run_glauber.C
@@ -1,4 +1,4 @@
-#include "runglauber_v3.2.C"
+//#include "runglauber_v3.2.C"
 
 void Run_glauber(){
 
@@ -7,8 +7,10 @@ void Run_glauber(){
 
 	bool bHIST = true;
 
-	//runAndOutputLemonTree(10, 0.5, "Pb", "Pb", 68.0, 0.4, 0.0, 2.0, 1, "MCGlauber-PbPb-5.02TeV-b0-2fm.root");
-	runAndOutputLemonTree(50, 0.4, "He3", "Au", 42.0, 0.4, 0.0, 2.0, bHIST, "MCGlauber-He3Au-200GeV-b0-2fm.root");
+	//runAndOutputLemonTree(100, 0.5, "Pb", "Pb", 68.0, 0.4, 4.0, 6.0, 1, "MCGlauber-PbPb-5.02TeV-b4-6fm.root");
+	//runAndOutputLemonTree(100, 0.5, "Pb", "Pb", 68.0, 0.4, 6.0, 8.0, 1, "MCGlauber-PbPb-5.02TeV-b6-8fm.root");
+	runAndOutputLemonTree(100, 0.5, "p", "Pb", 68.0, 0.4, 0.0, 2.0, 1, "MCGlauber-pPb-5.02TeV-b0-2fm.root");
+	//runAndOutputLemonTree(50, 0.4, "He3", "Au", 42.0, 0.4, 0.0, 2.0, bHIST, "MCGlauber-He3Au-200GeV-b0-2fm.root");
 
 
 }
