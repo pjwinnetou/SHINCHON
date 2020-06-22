59a75735a2f9690fd33b172694ac47524c69b0a7
Sanghoon Lim <shlim1986@gmail.com>
2020-06-22 19:40:37 +0900

    Drop the low energy check

--- Software/SONIC-KIAF/input/Hist2Txt.C
+++ Software/SONIC-KIAF/input/Hist2Txt.C
@@ -5,19 +5,21 @@ const bool saveQuarkDists = false; // TODO: whether to save wounded nucleon dist
 // Output file from runglauber should be called "outFile_${system}_${firstEvent}_${lastEvent}.root".
 const string name_system = "PbPb"; //TODO: e.g. "He3Au" or "pPb". For accessing and saving the right files/directories.
 const int energy = 5020;
-const int bmin = 6;
-const int bmax = 8;
+const int bmin = 0;
+const int bmax = 1;
 const int firstEvent = 0;
-const int lastEvent = 20;
-const int scaler = 2;
+const int lastEvent = 100;
+const int scaler = 5;
 
 // TODO: select an energy scaling value
 //const double e0 = (scaler*0.1+0.1)*0.00150022 * TMath::Power(140.*max_x/(5.*nbins), 4); // 200 GeV (RHIC) 
-const double e0 = (scaler*0.1+0.3)*0.00150022 * TMath::Power(140.*max_x/(5.*nbins), 4); // >5 TeV (LHC) 
+//const double e0 = (scaler*0.1+0.3)*0.00150022 * TMath::Power(140.*max_x/(5.*nbins), 4); // >5 TeV (LHC) 
+//const double e0 = (scaler*0.1+0.6)*0.00150022 * TMath::Power(140.*max_x/(5.*nbins), 4); // >5 TeV (LHC) //b 12-13 fm 
+const double e0 = (scaler*0.1+1.2)*0.00150022 * TMath::Power(140.*max_x/(5.*nbins), 4); // >5 TeV (LHC) //PbPb b 0-1 fm 
 
 void Hist2Txt () {
 
-	gSystem->Exec(Form("mkdir -p initedFiles_%s_%dGeV_b%d_%dfm",name_system.c_str(),energy,bmin,bmax));
+	gSystem->Exec(Form("mkdir -p initedFiles_%s_%dGeV_b%d_%dfm_scaler%03d",name_system.c_str(),energy,bmin,bmax,scaler));
 
 	// inFile contains the histogram with the wounded nucleon distribution for all events.
 	TFile* inFile = new TFile(Form("MCGlauber-%s-%dGeV-b%d-%dfm.root",name_system.c_str(),energy,bmin,bmax), "READ");
@@ -27,21 +29,25 @@ void Hist2Txt () {
   double max = 0; // for finding the maximum 
 	for (int eventNum = firstEvent; eventNum < lastEvent; eventNum++) {
 		TH2D* initedHist = (TH2D*)inFile->Get(Form("inited_event%i",eventNum));
-		initedHist->Scale (e0);
+		//initedHist->Scale (e0);
 
 		ofstream outFile;
 		char outFileName[500];
-		sprintf(outFileName, "./initedFiles_%s_%dGeV_b%d_%dfm/event%d.dat",name_system.c_str(),energy,bmin,bmax,eventNum);
+		sprintf(outFileName, "./initedFiles_%s_%dGeV_b%d_%dfm_scaler%03d/event%d.dat",name_system.c_str(),energy,bmin,bmax,scaler,eventNum);
 		outFile.open(outFileName);
 
     for (int xbin = 1; xbin <= nbins; xbin++) {
       for (int ybin = 1; ybin <= nbins; ybin++) {
 				double content = initedHist->GetBinContent(xbin, ybin); 
 
-				if (fabs(content)<1e-10){
-					content = 1e-10;
-					initedHist->SetBinContent(xbin, ybin, content);
+				//if ( content<0.001 ){
+				if ( 0 ){
+					initedHist->SetBinContent(xbin, ybin, 0.001*e0);
+				}else{
+					initedHist->SetBinContent(xbin, ybin, content*e0);
 				}
+
+				content = initedHist->GetBinContent(xbin, ybin); 
 
         outFile << content << "\t";
         if (content > max) max = content;
@@ -53,7 +59,7 @@ void Hist2Txt () {
     if (saveQuarkDists) {
      // outRootFile copies the histogram with the distribution of wounded nucleons for a single event.
      // This can be used by diffusion.C in generating the initial positions of heavy qqbar pairs.
-			TFile* outRootFile = new TFile(Form("./initedFiles_%s_%dGeV_b%d_%dfm/event%d.root",name_system.c_str(),energy,bmin,bmax,eventNum), "RECREATE");
+			TFile* outRootFile = new TFile(Form("./initedFiles_%s_%dGeV_b%d_%dfm_scaler%03d/event%d.root",name_system.c_str(),energy,bmin,bmax,scaler,eventNum), "RECREATE");
      outRootFile->cd();
      initedHist->Write();
     }
