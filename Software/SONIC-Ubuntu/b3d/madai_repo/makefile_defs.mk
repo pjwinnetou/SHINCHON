25c6f58040bbf54f05ca6bdd193ba606d4927f1a
Sanghoon Lim <shlim1986@gmail.com>
2021-01-09 11:36:52 +0900

    fix some paths for compile

--- Software/SONIC-Ubuntu/b3d/madai_repo/makefile_defs.mk
+++ Software/SONIC-Ubuntu/b3d/madai_repo/makefile_defs.mk
@@ -2,13 +2,13 @@
 B3DMAINDIR = $(PWD)
 MADAI_REPO = $(B3DMAINDIR)/madai_repo
 #prefix of .../rhic directory (root of madai installation)
-MADAI_GSLPATH = /usr/local/include/gsl
+MADAI_GSLPATH = /usr/include/gsl
 #where gnu scientific library is installed
 MADAI_X11PATH = /usr
 #root of X11 installation
-MADAI_HDF5_HOME = /usr/local
+MADAI_HDF5_HOME = /home/shlim/Work/SHINCHON/Software/Others/hdf5-1.10.6/install
 #root of HDF5 installation
-MADAI_CPP = $(shell which g++)
+MADAI_CPP = $(shell which g++-4.8)
 #compiler
 MADAI_INSTALLDIR = $(B3DMAINDIR)/install
 #location of where you want things installed
